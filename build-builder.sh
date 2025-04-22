50ba936ca9a29938aa5cdc54f7e4fdf7ef9defcc
Greg Martyn <greg@reflexions.co>
2025-04-22 12:49:32 -0400

    do artifact registry auth, update build-builder.sh to build both architectures

--- build-builder.sh
+++ build-builder.sh
@@ -4,7 +4,7 @@ set -x #echo on
 # shouldn't generally need this because it's built automatically on git push but this will do a full rebuild on demand
 
 push=${PUSH-0} # by default don't push. Pass PUSH=1 env var to push.
-no_cache=${NO_CACHE---no-cache} # by default don't use cache. Pass NO_CACHE= env var to enable caching
+no_cache=${NO_CACHE-} # by default use cache. Pass NO_CACHE=--no-cache env var to disable caching
 
 echo "pushing?: $push"
 echo "no cache?: $no_cache"
@@ -13,12 +13,19 @@ image1=us-central1-docker.pkg.dev/reflexions-ci-builder/ci-builder/ci-builder:ma
 # historical releases
 image2=us-central1-docker.pkg.dev/reflexions-ci-builder/ci-builder/ci-builder:$(date +"%Y-%m-%d")
 
-docker build $no_cache --pull \
-	-t "$image1" \
-	-t "$image2" \
-	. || exit 1
+platform="linux/amd64,linux/arm64"
 
 if [[ "$push" == "1" ]]; then
-	docker push $image1 || { echo "push $image1 failed"; exit 1; }
-	docker push $image2 || { echo "push $image2 failed"; exit 1; }
+	push_arg='--push'
+else
+	push_arg=''
 fi
+
+set -x # echo on
+exec docker buildx build $no_cache --pull \
+	--progress plain \
+	$push_arg \
+	--platform "${platform}" \
+	-t "$image1" \
+	-t "$image2" \
+	.
